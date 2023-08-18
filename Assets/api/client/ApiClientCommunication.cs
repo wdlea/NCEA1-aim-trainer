3ae4878bb0438fad6de7c9632e1fb30e9a706a05
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-19 11:52:19 +1200

    Started working on client

---
+++ Assets/api/client/ApiClientCommunication.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+namespace api
+{
+    public static partial class Client
+    {
+        internal static void Send(Packet packet)
+        {
+
+        }
+    }
+
+    internal enum PacketType
+    {
+        Error = 'E',
+
+        ServerBoundFrame = 'f',
+        ClientBoundFrameResponse = 'F',
+
+
+    }
+
+    [Serializable]
+    internal struct Packet
+    {
+        public readonly PacketType type;
+
+        public readonly string message;
+
+        public Packet(PacketType type, string message)
+        {
+            this.type = type;
+            this.message = message;
+        }
+
+        public Packet(byte[] packet) : this(
+            (PacketType)(char)packet[0], 
+            Encoding..GetString(packet[1..])) { }
+
+        public static Packet FromObject(PacketType type, object message)
+        {
+            return new Packet(type, JsonUtility.ToJson(message));
+        }
+
+        public byte[] ToBytes()
+        {
+
+        }
+    }   
+}
