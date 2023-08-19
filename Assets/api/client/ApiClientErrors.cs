0af8463ab68f756d9f54b7cec9e757b99d6512e6
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-19 13:10:38 +1200

    Added support for recieving and transmitting packets from client

---
+++ Assets/api/client/ApiClientErrors.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace api
+{
+    /// <summary>
+    /// The packet recieved is too big, this could be becuase it does not contain delimeters
+    /// </summary>
+    public class OversizedPacketException: System.Exception { }
+}
