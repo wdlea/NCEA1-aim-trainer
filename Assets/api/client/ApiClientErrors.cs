5e2f98ffc81c91b59e1fe0f891d0d8bcb713ce8d
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-20 16:37:18 +1200

    Finished first draft for multiplayer, awaiting names.

--- Assets/api/client/ApiClientErrors.cs
+++ Assets/api/client/ApiClientErrors.cs
@@ -7,4 +7,9 @@ namespace api
     /// The packet recieved is too big, this could be becuase it does not contain delimeters
     /// </summary>
     public class OversizedPacketException: System.Exception { }
+
+    /// <summary>
+    /// The packet recieved was not of the expected type, this indicates a larger error present in the server
+    /// </summary>
+    public class UnexpectedPacketException : System.Exception { }
 }
