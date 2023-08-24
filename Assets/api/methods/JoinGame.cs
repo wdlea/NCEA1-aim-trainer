6b5dffa72c4aecaa88d2e08d218f0b6f9e563754
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-25 08:29:54 +1200

    Added SendFrame

--- Assets/api/methods/JoinGame.cs
+++ Assets/api/methods/JoinGame.cs
@@ -1,3 +1,5 @@
+using api.objects;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,24 +23,53 @@ namespace api {
                 expectedType = PacketType.ClientBoundJoinResponse,
                 onResponse = (Packet p) =>
                   {
-                      switch (p.type)
+                      if(p.type == PacketType.ClientBoundJoinResponse)
                       {
-                          case PacketType.ClientBoundJoinResponse:
-                              {
-                                  promise.Fulfil(true);
-                                  break;
-                              }
-                          default:
-                              {
-                                  promise.Fail(new UnexpectedPacketException());
-                                  break;
-                              }
+                          promise.Fulfil(true);
+                      }
+                      else
+                      {
+                          promise.Fail(new UnexpectedPacketException());
                       }
                   }
             };
 
             Client.EnqueueSend(packet, ticket);
 
+            return promise;
+        }
+
+        /// <summary>
+        /// Sends a frame
+        /// </summary>
+        /// <param name="frame">The frame to send</param>
+        /// <returns>A promise that resolves to the game state</returns>
+        public static Promise<Game> SendFrame(Frame frame)
+        {
+            Promise<objects.Game> promise = new Promise<Game>();
+
+            Packet packet = Packet.FromObject(PacketType.ServerBoundFrame, frame);
+
+            ClaimTicket ticket = new ClaimTicket
+            {
+                expectedType = PacketType.ClientBoundFrameResponse,
+                onResponse = (Packet p) =>
+                  {
+                      if(p.type == PacketType.ClientBoundFrameResponse)
+                      {
+                          try
+                          {
+                              promise.Fulfil(JsonUtility.FromJson<Game>(p.message));
+                          }catch(Exception e){
+                              promise.Fail(e);
+                          }
+                      }
+                      else
+                      {
+                          promise.Fail(new UnexpectedPacketException());
+                      }
+                  }
+            };
             return promise;
         }
     }
