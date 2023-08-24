eafe33daddddc70257efa8906b95c3fdb1eeb039
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-25 11:36:27 +1200

    Restructured files

---
+++ Assets/api/methods/SendFrame.cs
@@ -0,0 +1,45 @@
+﻿using api.objects;
+using System;
+using UnityEngine;
+
+namespace api
+{
+    public static partial class Methods
+    {
+        /// <summary>
+        /// Sends a frame
+        /// </summary>
+        /// <param name="frame">The frame to send</param>
+        /// <returns>A promise that resolves to the game state</returns>
+        public static Promise<Game> SendFrame(Frame frame)
+        {
+            Promise<objects.Game> promise = new Promise<Game>();
+
+            Packet packet = Packet.FromObject(PacketType.ServerBoundFrame, frame);
+
+            ClaimTicket ticket = new ClaimTicket
+            {
+                expectedType = PacketType.ClientBoundFrameResponse,
+                onResponse = (Packet p) =>
+                {
+                    if (p.type == PacketType.ClientBoundFrameResponse)
+                    {
+                        try
+                        {
+                            promise.Fulfil(JsonUtility.FromJson<Game>(p.message));
+                        }
+                        catch (Exception e)
+                        {
+                            promise.Fail(e);
+                        }
+                    }
+                    else
+                    {
+                        promise.Fail(new UnexpectedPacketException());
+                    }
+                }
+            };
+            return promise;
+        }
+    }
+}
