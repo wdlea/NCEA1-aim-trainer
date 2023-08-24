ae5b3b1167fe9cc3d20ddfd766d7b41ee5c4b399
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-25 08:35:04 +1200

    Added SetName

--- Assets/api/methods/JoinGame.cs
+++ Assets/api/methods/JoinGame.cs
@@ -72,5 +72,37 @@ namespace api {
             };
             return promise;
         }
+
+        /// <summary>
+        /// Sets the player name, DO NOT apply 
+        /// the changed name before the promise resolves
+        /// the server is written to obey FIFO order.
+        /// </summary>
+        /// <param name="name">The name to set</param>
+        /// <returns>A promise resolving to the set name</returns>
+        public static Promise<string> SetName(string name)
+        {
+            Promise<string> promise = new Promise<string>();
+
+            Packet packet = new Packet(PacketType.ServerBoundName, name);
+
+            ClaimTicket ticket = new ClaimTicket
+            {
+                expectedType = PacketType.ClientBoundNameResponse,
+                onResponse = (Packet p) =>
+                  {
+                      if (p.type == PacketType.ClientBoundNameResponse)
+                      {
+                          promise.Fulfil(p.message);
+                      }
+                      else
+                      {
+                          promise.Fail(new UnexpectedPacketException());
+                      }
+                  }
+            };
+
+            return promise;
+        }
     }
 }
