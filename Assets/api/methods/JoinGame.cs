77ef15484ff72512a8ecffbf1dc1575696bfbf98
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-25 08:21:15 +1200

    Added JoinGame method, returning a promise

--- Assets/api/methods/JoinGame.cs
+++ Assets/api/methods/JoinGame.cs
@@ -5,9 +5,41 @@ using UnityEngine;
 namespace api {
     public static partial class Methods
     {
-        static Promise<bool> JoinGame()
+        /// <summary>
+        /// Joins a game.
+        /// </summary>
+        /// <param name="code">The code to join the game with</param>
+        /// <returns>A promise, will resolve to true if it was sucessful.</returns>
+        public static Promise<bool> JoinGame(string code)
         {
+            Promise<bool> promise = new Promise<bool>();
 
+            Packet packet = new Packet(PacketType.ServerBoundJoin, code);
+
+            ClaimTicket ticket = new ClaimTicket
+            {
+                expectedType = PacketType.ClientBoundJoinResponse,
+                onResponse = (Packet p) =>
+                  {
+                      switch (p.type)
+                      {
+                          case PacketType.ClientBoundJoinResponse:
+                              {
+                                  promise.Fulfil(true);
+                                  break;
+                              }
+                          default:
+                              {
+                                  promise.Fail(new UnexpectedPacketException());
+                                  break;
+                              }
+                      }
+                  }
+            };
+
+            Client.EnqueueSend(packet, ticket);
+
+            return promise;
         }
     }
 }
