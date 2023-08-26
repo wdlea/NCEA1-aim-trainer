32b7885162ca35157eba5c6f9114fe110b850115
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-26 14:37:22 +1200

    Started new layout

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,18 +1,24 @@
+using api;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
+
+    [SerializeField] surrogate surrogate;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(surrogate);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Client.IsConnected)
+        {
+            Client.JoinServer(surrogate);//blocks until joined so it will work or throw error
+        }
     }
 }
