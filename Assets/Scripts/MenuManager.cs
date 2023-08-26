0fac4836198151069e00c03f39700e801760ed39
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-26 16:04:19 +1200

    Finished first draft for new system.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,12 +1,25 @@
 using api;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+#nullable enable
 
 public class MenuManager : MonoBehaviour
 {
+    Promise<string>? namePromise;
+    Promise<bool>? joinPromise;
+    Promise<string>? hostPromise;
 
     [SerializeField] surrogate surrogate;
+
+    [SerializeField] private InputField nameInput;
+    [SerializeField] private InputField codeInput;
+
+    public static bool CanJoin => GameManager.myName.Length > 0 && Client.IsConnected;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +33,59 @@ public class MenuManager : MonoBehaviour
         {
             Client.JoinServer(surrogate);//blocks until joined so it will work or throw error
         }
+
+        if(namePromise != null && namePromise.Finished)
+        {
+            if (namePromise.Get(out string? playerName) is Exception e)
+            {
+                throw e;
+            }
+            else if(playerName is not null) GameManager.myName = playerName;
+        }
+
+        if(joinPromise != null && joinPromise.Finished)
+        {
+            if(joinPromise.Get(out bool success) is Exception e)
+            {
+                throw e;
+            }
+            else if(success)
+            {
+                //do shit
+            }
+            else
+            {
+                throw new Exception("Achievement unlocked: How did we get here?");//undefined behavior
+            }
+        }
+
+        if(hostPromise != null && hostPromise.Finished)
+        {
+            if (hostPromise.Get(out string? code) is Exception e)
+            {
+                throw e;
+            }
+            else if(code is not null)
+            {
+                //do shit
+            }
+            else
+            {
+                throw new Exception("Achievement unlocked: How did we get here?");//undefined behavior
+            }
+        }
+    }
+
+    public void ApplyName()
+    {
+        namePromise = Methods.SetName(nameInput.text);
+    }
+    public void JoinGame()
+    {
+        joinPromise = Methods.JoinGame(codeInput.text);
+    }
+    public void HostGame()
+    {
+        hostPromise = Methods.CreateGame();
     }
 }
