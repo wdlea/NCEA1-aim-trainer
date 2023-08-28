856e93a4ca52b015e88bf09917ad8daaa63e18d6
wdlea <83631101+wdlea@users.noreply.github.com>
2023-08-28 19:52:36 +1200

    Added C# docstrings.

---
+++ Assets/api/methods/HandleBroadcast.cs
@@ -0,0 +1,16 @@
+﻿namespace api
+{
+    public static partial class Methods
+    {
+        /// <summary>
+        /// Handles a broadcast packet
+        /// </summary>
+        /// <param name="newPacket">The broadcast packet, 
+        /// note that this will be transfomed to omit the
+        /// first 'B'</param>
+        internal static void HandleBroadcast(Packet newPacket)
+        {
+            //do stuff with packet
+        }
+    }
+}
