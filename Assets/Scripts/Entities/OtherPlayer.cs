b25c3b09de160ee79c930913a2eb9f2e50afcb57
wdlea <83631101+wdlea@users.noreply.github.com>
2023-09-10 14:17:39 +1200

    Added null check to onTargetSpawn and made enemies have a semi-random colour.

--- Assets/Scripts/Entities/OtherPlayer.cs
+++ Assets/Scripts/Entities/OtherPlayer.cs
@@ -8,6 +8,9 @@ public class OtherPlayer : Prop {
     [SerializeField] private Text scoreText;
     [SerializeField] private Text nameText;
 
+    [SerializeField] private Color[] colours;
+    [SerializeField] private Image icon;
+
     private api.objects.Player player;
     
     public api.objects.Player Player
@@ -17,6 +20,25 @@ public class OtherPlayer : Prop {
             Frame = value;
             player = value;
         }
+    }
+
+    private void Start()
+    {
+        icon.color = RandomChoice(colours);
+    }
+
+    /// <summary>
+    /// Chooses randomly
+    /// </summary>
+    /// <typeparam name="T">The type of item to choose from.</typeparam>
+    /// <param name="choices">All the possible choices</param>
+    /// <returns>The choice</returns>
+    public T RandomChoice<T>(IList<T> choices)
+    {
+        int count = choices.Count;
+        int choiceIndex = Random.Range(0, count);
+
+        return choices[choiceIndex];
     }
 
     private void Update()
