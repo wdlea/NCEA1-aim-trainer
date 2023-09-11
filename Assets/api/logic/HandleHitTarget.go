63daf0c2751887cf8e93c5b2b57a1cc705490659
William L <83631101+wdlea@users.noreply.github.com>
2023-09-11 19:26:23 +1200

    Added broadcast 'H' to server side code

--- Assets/api/logic/HandleHitTarget.go
+++ Assets/api/logic/HandleHitTarget.go
@@ -30,6 +30,15 @@ func HandleHitTarget(pak []byte, user *Player) (response []Packet, doTerminate b
 	if !hit {
 		returnValue = 0
 		fmt.Println("Target was attempted to be destroyed that was not present in game") //dont error out, but log it becuase that could help me deubug
+	} else {
+		//broadcast that the target is now hit
+
+		user.Game.SendBroadcastAll(
+			Packet{
+				Type:    'H',
+				Content: pak, //if the packet resolved to a hit, this will be called and valid
+			},
+		)
 	}
 
 	user.Score += float64(returnValue)
